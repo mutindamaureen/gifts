b386a46f582f8473d445b2cc57ca7d77894174c7
Maureen tynda <mutindamaureen05@gmail.com>
2025-02-12 00:16:33 +0300

    nixpacks update

--- gift_shop/settings.py
+++ gift_shop/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
-    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8000"
+    "https://shop2gift.netlify.app",
 ]
 
 MIDDLEWARE = [
